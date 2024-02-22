f1ccc1ac475dff3d8787caf18400afdb3496d466
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2024-02-22 03:04:21 +0300

    added Admin Layout

---
+++ Restaurant.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant.WebUI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("[area]/[controller]/[action]/{id?}")]
+	public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
